42336e35ccf037169182b0ce02cfbbd76e02acfc
Niamh <niamh.linguist@gmail.com>
2024-12-16 19:12:55 +0200

    second test

---
+++ tests/String.utils.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/test'
+import { capitalize } from '../utils/string.utils'
+
+test('it should capitalize a string', () => {
+  const actual = capitalize('hello')
+  const expected = 'Hello'
+
+  expect(actual).toBe(expected)
+})
