aa0665188989ac4f8f2461b8d792eab92c29c582
Niamh <niamh.linguist@gmail.com>
2024-12-17 00:37:27 +0200

    more componenent tests

--- tests/components/String.spec.ts
+++ tests/components/String.spec.ts
@@ -6,3 +6,13 @@ test('should work', async ({ mount }) => {
   const result = component.getByTestId('string-result')
   await expect(result).toHaveText(/result/i)
 })
+
+test('should capitalize a string', async ({ mount }) => {
+  const component = await mount(String)
+  const input = component.locator('input')
+  await input.fill('hello')
+  const button = component.getByRole('button', { name: /capitalise/i })
+  await button.click()
+  const result = component.getByTestId('string-result')
+  expect(await result.textContent()).toContain('Hello')
+})
--- tests/components/Sum.spec.ts
+++ tests/components/Sum.spec.ts
@@ -6,3 +6,14 @@ test('should work', async ({ mount }) => {
   const result = component.getByTestId('sum-result')
   await expect(result).toHaveText(/result/i)
 })
+
+test('should add two numbers', async ({ mount }) => {
+  const component = await mount(Sum)
+  const firstInput = component.locator('input[name="num_a"]')
+  await firstInput.fill('1')
+  const secondInput = component.locator('input[name="num_b"]')
+  await secondInput.fill('2')
+  await component.getByRole('button', { name: 'Calculate' }).click()
+  const result = component.getByTestId('sum-result')
+  expect(await result.textContent()).toContain('3')
+})
